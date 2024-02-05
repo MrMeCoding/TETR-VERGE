48fd0f5c450359765241c1abb78e9dedc18d6688
MrMe <76573108+MrMeCoding@users.noreply.github.com>
2024-02-05 07:30:10 -0500

    Update preload.js

--- v0.0.2/VERGE-Browser/preload.js
+++ v0.0.2/VERGE-Browser/preload.js
@@ -1,24 +1,3 @@
-window.IS_ELECTRON = true;
-window.IPC = require('electron').ipcRenderer;
-window.BASEBOARD = undefined;
-window.REFRESH_RATE = 60;
-
-const si = require('systeminformation');
-
-
-si.baseboard()
-	.then((data) => {
-		window.BASEBOARD = data.serial;
-	})
-	.catch((err) => { console.error(err); });
-
-si.graphics()
-	.then((data) => {
-		data.displays.forEach((d) => {
-			window.REFRESH_RATE = Math.max(window.REFRESH_RATE, d.currentRefreshRate || 60);
-		});
-	})
-	.catch((err) => { console.error(err); });
 // VERGE
 
 // Config
